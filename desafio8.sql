7d889cb91471b07d6d92aa91784be3b5db99de25
martinsph <ph.mmartins@gmail.com>
2021-10-04 17:19:03 -0300

    FIX req 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,13 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-	  BEFORE DELETE ON usuarios
-	  FOR EACH ROW
+BEFORE DELETE ON usuarios
+FOR EACH ROW
 BEGIN
-	  DELETE FROM SpotifyClone.historico_cancoes
-    WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM SpotifyClone.seguidos
-    WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_cancoes 
+WHERE
+    usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguidos 
+WHERE
+    usuario_id = OLD.usuario_id;
 END
 $$ DELIMITER ;
