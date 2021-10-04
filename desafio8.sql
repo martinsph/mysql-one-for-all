57bb657967601c1aca71989c94c999d9bb261ca2
martinsph <ph.mmartins@gmail.com>
2021-10-04 16:43:08 -0300

    add req 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+	BEFORE DELETE ON usuarios
+	FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.historico_cancoes
+    WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.seguidos
+    WHERE usuario_id = OLD.usuario_id;
+END
+$$ DELIMITER ;
